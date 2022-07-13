4b4a24310a460fa43e9d932c97d5fcbc8363611b
shrey <shreyash966977@gmail.com>
2022-07-14 00:58:28 +0530

    updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    return "CI CD PIPELINE has been established"
 
 
 if __name__=="__main__":
