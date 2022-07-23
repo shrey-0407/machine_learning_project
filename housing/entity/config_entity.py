cf61b3a22c191137985590b3bbff3db4ca967057
shrey <shreyash966977@gmail.com>
2022-07-23 12:52:19 +0530

    updated

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -21,3 +21,7 @@ ModelEvaluationConfig = namedtuple("ModelEvaluationConfig", ["model_evaluation_f
 ModelPusherConfig = namedtuple("ModelPusherConfig", ["export_dir_path"])
 
 TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
+
+
+
+
